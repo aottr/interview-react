971562cec28541378be3c9f20418ea217aff610f
Dustin KROEGER <dkroeger@exomind.fr>
2024-12-13 13:50:20 +0100

    add react

---
+++ react-vanilla/README.md
@@ -0,0 +1,15 @@
+# React Interview (React + TypeScript + Vite)
+
+This is a starter template for React + TypeScript + Vite for the [Interview React](https://github.com/aottr/interview-react) project.
+
+## Getting Started
+
+First, run the development server:
+
+```bash
+npm run dev
+```
+
+Open [http://localhost:5173](http://localhost:5173) with your browser to see the result.
+
+You can start editing the page by modifying `main.tsx`. The page auto-updates as you edit the file.
---
+++ react-vanilla/src/routes/root.tsx
@@ -0,0 +1,16 @@
+import { Link, Outlet } from "react-router";
+
+export default function Root() {
+  return (
+    <div className="flex flex-col min-h-screen">
+        <div className="navbar bg-base-200">
+            <div className="flex-1">
+                <Link to="/" className="btn btn-ghost text-xl">React Blog Interview</Link>
+            </div>
+        </div>
+        <main className="flex-grow container mx-auto p-4">
+            <Outlet />
+        </main>
+    </div>
+  )
+}
